030c8033f746a1f76463036b62a743bf360e8321
fiuwegfiu <fiuwegfiulkvc@gmail.com>
2026-08-04 22:04:49 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+import random
+import string
+import os
+
+app = Flask(__name__)
+
+
+# =====================
+# SECURITY
+# =====================
+
+API_KEY = os.environ.get("API_KEY")
+
+
+def verify_api_key():
+    auth = request.headers.get("Authorization")
+
+    if auth != API_KEY:
+        return False
+
+    return True
+
+
+# =====================
+# GENERATORS
+# =====================
+
+def numbers(amount):
+    return ''.join(random.choice(string.digits) for _ in range(amount))
+
+
+def letters(amount):
+    return ''.join(random.choice(string.ascii_uppercase) for _ in range(amount))
+
+
+# =====================
+# JOIN CODE
+# =====================
+
+@app.route("/generate/join")
+def join():
+
+    if not verify_api_key():
+        return jsonify({"error": "Unauthorized"}), 401
+
+    code = f"{numbers(3)}{letters(2)}{numbers(3)}{letters(1)}"
+
+    return jsonify({
+        "code": code
+    })
+
